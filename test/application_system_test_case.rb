fba6e5912bfe9b4d02e80e0cafb45be958cda0bb
Andrew Mason <andrewmcodes@protonmail.com>
2019-10-11 17:35:53 -0400

    try to fix ci tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,20 +1,22 @@
 require "test_helper"
 require "minitest/rails/capybara"
 
-Capybara.app_host = "http://#{ENV["TEST_APP_HOST"]}:#{ENV["TEST_PORT"]}"
-Capybara.javascript_driver = :selenium
-Capybara.run_server = false
+if ENV["SELENIUM_HOST"] == "selenium"
+  Capybara.app_host = "http://#{ENV["TEST_APP_HOST"]}:#{ENV["TEST_PORT"]}"
+  Capybara.javascript_driver = :selenium
+  Capybara.run_server = false
 
-# Configure the firefox driver capabilities & register
-args = ["--no-default-browser-check", "--start-maximized", "--no-sandbox"]
-caps = Selenium::WebDriver::Remote::Capabilities.firefox("firefoxOptions" => {"args" => args})
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :remote,
-    url: "http://#{ENV["SELENIUM_HOST"]}:#{ENV["SELENIUM_PORT"]}/wd/hub",
-    desired_capabilities: caps
-  )
+  # Configure the firefox driver capabilities & register
+  args = ["--no-default-browser-check", "--start-maximized", "--no-sandbox"]
+  caps = Selenium::WebDriver::Remote::Capabilities.firefox("firefoxOptions" => {"args" => args})
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :remote,
+      url: "http://#{ENV["SELENIUM_HOST"]}:#{ENV["SELENIUM_PORT"]}/wd/hub",
+      desired_capabilities: caps
+    )
+  end
 end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
